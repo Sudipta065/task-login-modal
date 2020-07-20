72db0b85382ec675a904bccc9a1adfea33cb2da1
Sudipta Adhikary <sudipta80065@gmail.com>
2020-07-20 15:39:03 +0600

    Button Added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./styles/App.scss";
 
 const App = () => {
-  return <div>App component</div>;
+  return (
+    <div className="content">
+      <button className="btn btn-primary">Open Login Modal</button>
+    </div>
+  );
 };
 
 export default App;
